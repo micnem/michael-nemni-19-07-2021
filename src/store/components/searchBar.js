20d630d72035a1ca6d5e275649950187548b89ae
Michael <michaeln@rumble.co.il>
2021-07-26 17:20:42 +0300

    removing extra import

--- src/store/components/searchBar.js
+++ src/store/components/searchBar.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
-import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import { useWeatherStore } from "../index";
 import { observer } from "mobx-react";
