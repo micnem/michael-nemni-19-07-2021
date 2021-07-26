d8899e68267584938254dffc4105615c60902d09
Michael <michaeln@rumble.co.il>
2021-07-26 17:27:52 +0300

    fixing empty search

--- src/store/components/searchBar.js
+++ src/store/components/searchBar.js
@@ -1,25 +1,26 @@
-import React from 'react';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
+import React from "react";
+import { Button, TextField } from "@material-ui/core";
 import { useWeatherStore } from "../index";
 import { observer } from "mobx-react";
 
 export const SearchBar = observer(() => {
-const store = useWeatherStore();
+  const store = useWeatherStore();
 
   const setQuery = (event) => {
-    store.searchQuery = event.target.value
-  }
+    store.searchQuery = event.target.value;
+  };
   const search = () => {
-    store.search()
-  }
+    if (store.searchQuery) {
+      store.search();
+    }
+  };
   return (
     <div style={{ width: 300 }}>
-      <TextField
-      label="Outlined" variant="outlined" onChange={setQuery}
-      />
-      <Button onClick={search}> search </Button>
+      <TextField label="Search" variant="outlined" onChange={setQuery} />
+      <Button onClick={search} color="primary" variant="contained">
+        {" "}
+        search{" "}
+      </Button>
     </div>
   );
-})
-
+});
